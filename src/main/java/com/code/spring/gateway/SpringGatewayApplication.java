14c42bca1b290fcbd88e4a942f5531edec7c51a6
prthatip <prthatip@lin66012927.corp.capgemini.com>
2023-01-09 22:20:17 +0530

    base commit

---
+++ src/main/java/com/code/spring/gateway/SpringGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.code.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class SpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApplication.class, args);
+	}
+
+}
